21fad6c24f534a12e8ec36ffd771da3b51e6d355
SeberyDev <sebasmanager06@gmail.com>
2022-08-24 13:07:05 -0500

    Se añadio la base para los formularios de dar de baja o de alta a un estudiante, se cambio el color de la imagen fondo de blanco a azul para agregarle a la ultima tabla un fondo blanco

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // Checar si el usuario está autenticado con usuario y contraseña
 const isAuth = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      return next();
-    } else {
-      res.redirect("/");
-    }
-  };
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect("/");
+  }
+};
 
 // RUTAS GET -----------------------------------------------------------
 /* PÁGINA INICIAL ADMIN */
-router.get('/', isAuth, function(req, res, next) {
-    if(req.user.type == "student") res.redirect(`/dashboard?user=${req.user.student}`)
-    else res.send('Hola Admin');
+router.get("/", isAuth, function (req, res, next) {
+  if (req.user.type == "student")
+    res.redirect(`/dashboard?user=${req.user.student}`);
+  else res.render("admin", { title: "ADMIN" });
 });
-
 
 // RUTAS POST -----------------------------------------------------------
 
